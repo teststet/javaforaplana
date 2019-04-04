e2d14f679f90bddda9d8471573b7b2481aff0d81
testest <pavlikthebestonmars@gmail.com>
2019-04-05 01:40:06 +0300

    Добавил решение 3 и 4 задачи

--- src/main/java/lesson2_dz/dz2_first.java
+++ src/main/java/lesson2_dz/dz2_first.java
@@ -5,7 +5,9 @@ import java.util.*;
 public class dz2_first {
     public static void main(String[] args) {
     //getChetAndNechet(getRandomNumbers(9));
-    getMaxAndMin(getRandomNumbers(10));
+    //getMaxAndMin(getRandomNumbers(10));
+    //div3or9(getRandomNumbers(10));
+    div5and7(getRandomNumbers(1000));
     }
 
     public static int[] getRandomNumbers(int count) {
@@ -20,7 +22,7 @@ public class dz2_first {
         return res;
     }
 
-    //Задание 1
+    //(1)	Четные и нечетные числа.
     public static void getChetAndNechet (int[] arr) {
         for (int i = 0; i < arr.length; i++) {
             if((arr[i] % 2) == 0) {
@@ -29,7 +31,7 @@ public class dz2_first {
         }
     }
 
-    //Задание 2
+    //(2)	Наибольшее и наименьшее число.
     public static void getMaxAndMin (int[] arr) {
         int minCh = arr[0];
         int maxCh = arr[0];
@@ -39,5 +41,24 @@ public class dz2_first {
         }
         System.out.println("Минимальное число = " + minCh);
         System.out.println("Максимальное число = " + maxCh);
+    }
+
+    //(3)	Числа, которые делятся на 3 или на 9.
+    public static void div3or9 (int[] arr) {
+        System.out.println("Числа которые делятся на 3 или на 4");
+        for (int i = 0; i < arr.length; i++) {
+            if ((arr[i] % 3) == 0 || (arr[i] % 4) == 0)
+                System.out.println(arr[i]);
+        }
+
+    }
+    //(4)	Числа, которые делятся на 5 и на 7.
+    public static void div5and7 (int[] arr) {
+        System.out.println("Числа которые делятся на 5 и на 7");
+        for (int i = 0; i < arr.length; i++) {
+            if ((arr[i] % 5) == 0 && (arr[i] % 7) == 0)
+                System.out.println(arr[i]);
+        }
+
     }
 }
