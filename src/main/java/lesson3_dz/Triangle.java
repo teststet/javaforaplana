5143d120416ed4d369668e8373be402e43dcfc10
testest <pavlikthebestonmars@gmail.com>
2019-04-30 00:30:24 +0300

    Добавления коментариев

--- src/main/java/lesson3_dz/Triangle.java
+++ src/main/java/lesson3_dz/Triangle.java
@@ -9,6 +9,7 @@ public class Triangle {
     int b_side;
     int c_side;
 
+    //В конструкторе есть проверка на существование треугольника
     public Triangle (int a_side, int b_side, int c_side) {
         if(((a_side + b_side) > c_side) && ((a_side + c_side) > b_side)
                 && ((b_side + c_side) > a_side)){
@@ -16,6 +17,19 @@ public class Triangle {
             this.b_side = b_side;
             this.c_side = c_side;
 
+        }
+        else
+            System.out.println("Не существует треугольника с такими сторонами");
+    }
+    //Вершины треугольника класс Точка, для определения растояния между точками используется метод по высчитыванию растояния
+    //между двумя точками
+    public Triangle (Point a, Point b, Point c) {
+        if(((a.distance_between_two_points(b) + b.distance_between_two_points(c)) > c.distance_between_two_points(a))
+                && ((b.distance_between_two_points(b) + c.distance_between_two_points(c)) > a.distance_between_two_points(a))
+                && ((a.distance_between_two_points(b) + c.distance_between_two_points(c)) > b.distance_between_two_points(a))){
+            this.a_side = (int) a.distance_between_two_points(b);
+            this.b_side = (int) b.distance_between_two_points(c);
+            this.c_side = (int) c.distance_between_two_points(a);
         }
         else
             System.out.println("Не существует треугольника с такими сторонами");
@@ -58,18 +72,20 @@ public class Triangle {
         return this.a_side + this.b_side + this.c_side;
     }
 
+    //Площадь вычисляется по формуле Герона
     public double square () {
         double p = perimeter() / 2;
         //System.out.println(p);
         double s = Math.sqrt(p * (p - a_side) * (p - b_side) * (p - c_side));
         return new BigDecimal(s).setScale(3, RoundingMode.UP).doubleValue();
     }
-
+    // Метод по определению существования треугольника с задаными сторонами
     public boolean true_triangle() {
         return ((getA_side() + getB_side()) > getC_side()) && ((getA_side() + getC_side()) > getB_side())
                 && ((getB_side() + getC_side()) > getA_side());
     }
 
+    //Метод по определению типа треугольника
     public String type_triangle() {
         String type;
         if (getA_side() == getB_side() && getB_side() == getC_side() && getA_side() == getC_side())
--- src/main/java/lesson3_dz/Triangle_Main.java
+++ src/main/java/lesson3_dz/Triangle_Main.java
@@ -2,11 +2,72 @@ package lesson3_dz;
 
 public class Triangle_Main {
     public static void main(String[] args) {
-        Triangle[] t = new Triangle[2];
+        Triangle[] t = new Triangle[3];
         t[0] = new Triangle(3, 3, 3);
+        t[1] = new Triangle(3, 4, 5);
+        t[2] = new Triangle(3, 4, 3);
         System.out.println(t[0].toString());
         System.out.println(t[0].perimeter());
         System.out.println(t[0].square());
         System.out.println(t[0].type_triangle());
+        type(t);
+        mimmaxSq(t);
+
+
+    }
+    public  static void mimmaxSq (Triangle[] triangle) {
+        System.out.println("\nМаксимальная и минимальная площадь треугольников");
+        System.out.println("Площадь равносторонних треугольников: min = " + minSq(triangle,  "равносторонний") + "\tmax = " + maxSq(triangle, "равносторонний"));
+        System.out.println("Площадь равнобедренных треугольников: min = " + minSq(triangle, "равнобедренный") + "\tmax = " + maxSq(triangle, "равнобедренный"));
+        System.out.println("Площадь прямоугольных треугольников: min = " + minSq(triangle, "прямоугольный") + "\tmax = " + maxSq(triangle, "прямоугольный"));
+        System.out.println("Площадь произвольных треугольников: min = " + minSq(triangle, "произвольный") + "\tmax = " + maxSq(triangle, "произвольный"));
+    }
+
+    public static double minSq (Triangle[] triangle, String str) {
+        int i = 0;
+        double minSq = 0;
+        for(; i < triangle.length; i++) {
+            if (triangle[i].type_triangle().equals(str)) {
+                minSq = triangle[i].square();
+                break;
+            }
+        }
+        for(; i < triangle.length; i++) {
+            if(triangle[i].type_triangle().equals(str)) {
+                minSq = minSq > triangle[i].square() ?  triangle[i].square() : minSq;
+            }
+
+        }
+        return minSq;
+    }
+
+    public static double maxSq (Triangle[] triangle, String str) {
+        double maxSq = 0;
+        for(int i = 0; i < triangle.length; i++) {
+            if(triangle[i].type_triangle().equals(str)) {
+                maxSq = maxSq < triangle[i].square() ?  triangle[i].square() : maxSq;
+            }
+
+        }
+        return maxSq;
+    }
+    //Подсчет по количеству типов треугольников
+    public static void type(Triangle[] triangle) {
+        int equilateral = 0;
+        int isosceles = 0;
+        int rectangular = 0;
+        int arbitrary = 0;
+
+        for (int i = 0; i < triangle.length; i++){
+            if(triangle[i].type_triangle().equals("равносторонний")) equilateral++;
+            else if (triangle[i].type_triangle().equals("равнобедренный")) isosceles++;
+            else if (triangle[i].type_triangle().equals("прямоугольный")) rectangular++;
+            else arbitrary++;
+        }
+        System.out.println("\nКоличество треугольников по типу");
+        System.out.println("Равносторонних треугольников = " + equilateral);
+        System.out.println("Равнобедренных треугольников = " + isosceles);
+        System.out.println("Прямоугольных треугольников = " + rectangular);
+        System.out.println("Произвольных треугольников = " + arbitrary);
     }
 }
